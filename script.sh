e5af68b0f7612e4b3800228268611647217c32dc
vitalyansky <142103247+vitalyansky@users.noreply.github.com>
2024-04-26 21:18:19 +0300

    Script.sh

--- script.sh
+++ script.sh
@@ -1,52 +1,48 @@
 #!/bin/bash
 
-# Проверка, что переданы два аргумента
-if [ $# -ne 2 ]; then
-    echo "Использование: $0 входная_директория выходная_директория"
+# Проверка количества аргументов
+if [ "$#" -ne 2 ]; then
+    echo "Использование: $0 входная директория выходная директория"
     exit 1
 fi
 
-input_dir="$1"
-output_dir="$2"
+src_dir="$1"
+dest_dir="$2"
 
-# Проверка, существует ли входная директория
-if [ ! -d "$input_dir" ]; then
-    echo "Входная директория $input_dir не существует."
+# Проверка существования директорий
+if [ ! -d "$src_dir" ]; then
+    echo "Входная директория $src_dir не существует."
     exit 1
 fi
 
-# Проверка, существует ли выходная директория
-if [ ! -d "$output_dir" ]; then
-    echo "Выходная директория $output_dir не существует."
+if [ ! -d "$dest_dir" ]; then
+    echo "Выходная директория $dest_dir не существует."
     exit 1
 fi
 
-# Функция для копирвания файлов
-copy_file() {
-    local source_path="$1"
-    local destination_path="$2"
-    local filename=$(basename "$source_path")
-    local extension="${filename##*.}"
-    local name="${filename%.*}"
+# Функция для копирования файлов с проверкой дубликатов
+safe_copy() {
+    local src_path="$1"
+    local dest_path="$2"
+    local base_name=$(basename -- "$src_path")
+    local file_ext="${base_name##*.}"
+    local file_name="${base_name%.*}"
 
-    # Формирование начального пути
-    local counter=0
-    local new_filename="$filename"
-
-    # Проверка на совпадение имен
-    while [ -f "$output_dir/$new_filename" ]; do
-        # Увеличиваем счетчик и формируем новое имя файла с индексом
-        ((counter++))
-        new_filename="${name} ($counter).${extension}"
+    # Обработка совпадения имен файлов в целевой директории
+    local index=0
+    local new_file_name="$base_name"
+    while [ -f "$dest_dir/$new_file_name" ]; do
+        index=$((index + 1))
+        new_file_name="${file_name} ($index).$file_ext"
     done
 
-    # Копируем файл
-    cp "$source_path" "$output_dir/$new_filename"
+    # Копирование файла
+    cp -- "$src_path" "$dest_dir/$new_file_name"
 }
 
-# Находим все файлы в директории и её поддиректориях
-find "$input_dir" -type f | while read -r file; do
-    copy_file "$file" "$output_dir/$(basename "$file")"
+# Использование find с -print0 и read -d '' для обработки всех имен файлов
+find "$src_dir" -type f -print0 | while IFS= read -r -d '' file; do
+    safe_copy "$file" "$dest_dir/$(basename -- "$file")"
 done
 
-echo "Все файлы были скопированы в $output_dir"
+echo "Файлы скопированы в $dest_dir"
